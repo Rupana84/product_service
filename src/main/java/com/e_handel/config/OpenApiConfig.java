50264f9ebe3b0a0e4d00cbf5e20550e45606d80b
Rupana <sistajag@gmail.com>
2025-04-09 08:32:38 +0200

    Updated the openAPIconfig file

--- src/main/java/com/e_handel/config/OpenApiConfig.java
+++ src/main/java/com/e_handel/config/OpenApiConfig.java
@@ -1,10 +1,12 @@
 package com.e_handel.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,6 +16,9 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Product Service API")
                         .version("1.0")
-                        .description("API documentation for Order Service"));
+                        .description("API documentation for Product Service"))
+                .servers(List.of(
+                        new Server().url("https://productservice-production.up.railway.app")
+                ));
     }
 }
